abf4a223c3d7167b589b58b7e25a2edd35ace64b
MangoSteen0903 <sjlee990129@gmail.com>
2022-03-19 22:32:16 +0900

    Code Challenge -Struct Method

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func main(){
-	fmt.Println("Hello")
+type person struct {
+	name      string
+	birthDate int
+}
+
+func (p person) getKoreanAge() int {
+	currentYear := time.Now().Year()
+	koreanAge := currentYear - p.birthDate + 1
+
+	return koreanAge
+}
+func (p person) sayHello() {
+	kAge := p.getKoreanAge()
+	fmt.Printf("Hello! My name is %s and I'm %d", p.name, kAge)
+}
+
+func main() {
+	milky := person{"milky", 1997}
+	milky.sayHello()
 }
