3515343f38c1890e949da50e0a5dc4669d3df8f5
MangoSteen0903 <sjlee990129@gmail.com>
2022-03-19 23:55:53 +0900

    Barbone Blockchain /w Singleton Pattern

--- main.go
+++ main.go
@@ -2,26 +2,20 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	"github.com/mangosteen0903/go-coin/blockchain"
 )
 
-type person struct {
-	name      string
-	birthDate int
-}
-
-func (p person) getKoreanAge() int {
-	currentYear := time.Now().Year()
-	koreanAge := currentYear - p.birthDate + 1
-
-	return koreanAge
-}
-func (p person) sayHello() {
-	kAge := p.getKoreanAge()
-	fmt.Printf("Hello! My name is %s and I'm %d", p.name, kAge)
-}
-
 func main() {
-	milky := person{"milky", 1997}
-	milky.sayHello()
+	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
+		fmt.Println("")
+	}
 }
