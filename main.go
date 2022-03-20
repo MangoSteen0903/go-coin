be774153d8d64ecc3d6eadd1b2bf4c3828fb4863
MangoSteen0903 <sjlee990129@gmail.com>
2022-03-20 15:53:47 +0900

    Learning REST API

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL        string `json:"url"`
+	Method     string `json:"method"`
+	Desciption string `json:"description"`
+	Payload    string `json:"payload,omitempty"`
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:        "/",
+			Method:     "GET",
+			Desciption: "See Documentation",
+		},
+		{
+			URL:        "/blocks",
+			Method:     "GET",
+			Desciption: "Add a Block",
+			Payload:    "data:string",
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(data)
+}
 func main() {
-	//explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
